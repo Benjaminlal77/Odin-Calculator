419f047f30204964b8dfa1f42fa40b7dcb4f72b5
Benjaminlal77 <benjaminlal2004@gmail.com>
2023-01-30 04:46:37 -0500

    Add decimal button functionality

--- scripts.js
+++ scripts.js
@@ -4,18 +4,23 @@ const result = document.getElementById('result');
 buttons.forEach(button=>button.addEventListener('click',checkInput));
 
 function checkInput(e){
-  if (e.target.className==='number-button'){
-    result.textContent+=e.target.value;
+  let buttonPressed = e.target.className;
+  let input = e.target.value;
+  if (buttonPressed==='number-button'){
+    result.textContent+=input;
   } 
-  else if (e.target.className==='operator-button'){
+  else if (buttonPressed==='operator-button'){
     if (result.textContent!==''){
-      memory.textContent += result.textContent + ' ' + e.target.value + ' ';
+      memory.textContent += result.textContent + ' ' + input + ' ';
       result.textContent = '';
     } else {
       if (lastInputIsOperator()){
-        memory.textContent = memory.textContent.slice(0,-2)+e.target.value+' '
+        memory.textContent = memory.textContent.slice(0,-2)+input+' '
       }
     }
+  }
+  else if (buttonPressed==='decimal-button'){
+    result.textContent+=input;
   }
 }
 function lastInputIsOperator(){
